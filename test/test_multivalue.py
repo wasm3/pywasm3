57fd3064a749ffc68c76bd993a9bab858d4a54af
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2021-04-23 02:26:57 +0300

    test_multivalue_imported

--- test/test_multivalue.py
+++ test/test_multivalue.py
@@ -12,6 +12,18 @@ MV_SWAP_WASM = wat2wasm("""
 )
 """)
 
+MV_IMPORT_WASM = wat2wasm("""
+(module
+  (type $t0 (func (param i32 i32) (result i32 i32)))
+  (import "env" "swap" (func $env.swap (type $t0)))
+  (func (export "swap") (type $t0)
+    (get_local 0)
+    (get_local 1)
+    (call $env.swap)
+  )
+)
+""")
+
 def test_multivalue_swap():
     env = m3.Environment()
     rt = env.new_runtime(64)
@@ -24,5 +36,11 @@ def test_multivalue_swap():
     assert swap.call_argv('2', '1') == (1, 2)
 
 def test_multivalue_imported():
-    # TODO
-    pass
+    env = m3.Environment()
+    rt = env.new_runtime(64)
+    mod = env.parse_module(MV_IMPORT_WASM)
+    rt.load(mod)
+    mod.link_function("env", "swap", "ii(ii)", lambda a,b: (b,a))
+    swap = rt.find_function('swap')
+    assert swap(1, 2) == (2, 1)
+    assert swap(2, 1) == (1, 2)
